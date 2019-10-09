4e095c5e34af294995cc7b8db65f80fd4242d836
victorfg21 <32532774+victorfg21@users.noreply.github.com>
2019-10-08 23:34:31 -0300

    Estudo event-bind

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,10 +10,15 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
-  
+  public resposta: string;
+
   constructor() { console.log(this.frases) }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void
+  {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
 }
